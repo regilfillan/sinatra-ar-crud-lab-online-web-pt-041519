1a56c3fbcc36286790ab110fc8de36c1c2c0754f
Ryan Gilfillan <regilfillan@gmail.com>
2019-08-05 23:17:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{ @article.id }"
   end
   
-  get "/articles/:id" do
+  get "/articles/:id" do #read
     @article = Article.find(params[:id])
     erb :show
   end
